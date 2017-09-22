1e94b49da25dae3ab5202844c75e83a98f6623ce
david <davidaupin@gmail.com>
2017-09-22 10:41:28 -0400

    Inventory and Cell begining

---
+++ Assets/Scripts/Playmode/State/Inventory/Cell.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjetSynthese
+{
+    public class Cell
+    {
+
+        public Cell()
+    {
+
+    }
+
+        public virtual void SetItem(GameObject game)
+        {
+        }
+
+        public virtual Item GetItem()
+        {
+            return null;
+        }
+
+        public virtual void SetImage()
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Playmode/State/Inventory/Inventory.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjetSynthese
+{
+    public enum InventoryOf {Player, Item}
+
+    [AddComponentMenu("Game/State/Health")]
+    public class Inventory : GameScript
+    {
+
+        [SerializeField]
+        private InventoryOf inventoryOf;
+
+        private List<Cell> listInventory;
+
+        public void Add(GameObject game)
+        {
+            AddItemCellToInventory(game);
+            AddPlayerCellToInventory(game);
+        }
+
+        private void AddPlayerCellToInventory(GameObject game)
+        {
+            if (inventoryOf == InventoryOf.Player)
+            {
+                Cell cell = CreatePlayerCell(game);
+                listInventory.Add(cell);
+            }
+        }
+
+        private static Cell CreatePlayerCell(GameObject game)
+        {
+            Cell cell = new CellPlayer();
+            cell.SetItem(game);
+            cell.SetImage();
+            return cell;
+        }
+
+        private void AddItemCellToInventory(GameObject game)
+        {
+            if (inventoryOf == InventoryOf.Item)
+            {
+                Cell cell = CreateItemCell(game);
+                listInventory.Add(cell);
+            }
+        }
+
+        private static Cell CreateItemCell(GameObject game)
+        {
+            Cell cell = new CellItem();
+            cell.SetItem(game);
+            cell.SetImage();
+            return cell;
+        }
+
+        public void Remove(GameObject game)
+        {
+            if (inventoryOf == InventoryOf.Item)
+            {
+                listInventory.Remove(CreateItemCell(game));
+            }
+            if (inventoryOf == InventoryOf.Player)
+            {
+                listInventory.Remove(CreatePlayerCell(game));
+            }
+            
+        }
+    }
+}
+

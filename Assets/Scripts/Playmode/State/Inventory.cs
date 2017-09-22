86ff71002c7d0119f5060e0c74be083b16070205
david <davidaupin@gmail.com>
2017-09-22 09:44:45 -0400

    Item exist

---
+++ Assets/Scripts/Playmode/State/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjetSynthese
+{
+    public enum InventoryOf {Player, Item}
+
+    [AddComponentMenu("Game/State/Health")]
+    public class Inventory : GameScript
+    {
+
+        [SerializeField]
+        private InventoryOf inventoryOf;
+
+        private List<GameObject> listInventory;
+
+	    // Use this for initialization
+	    void Start () {
+		
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+		
+	    }
+
+        public void Add(GameObject game)
+        {
+            listInventory.Add(game);
+        }
+
+        public void Remove(GameObject game)
+        {
+            listInventory.Remove(game);
+        }
+    }
+}
+

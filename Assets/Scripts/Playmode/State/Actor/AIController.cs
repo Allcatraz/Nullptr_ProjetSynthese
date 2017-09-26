bae00354be0904d0eca40d6d489fbf384dc9bd0a
FredGag <crimsonnethys@hotmail.com>
2017-09-26 14:52:32 -0400

    update Aicontroler

--- Assets/Scripts/Playmode/State/Actor/AIController.cs
+++ Assets/Scripts/Playmode/State/Actor/AIController.cs
@@ -12,6 +12,9 @@ namespace ProjetSynthese
         public bool OpponentTargetDestinationIsKnown { get; set; }
         public bool ItemTargetDestinationIsKnown { get; set; }
 
+        public enum MoveTarget { Item,Map,Opponent };
+        public MoveTarget AIMoveTarget { get; set; }
+
         public enum SpeedLevel { Walking, Jogging, Running,Swimming };
         public SpeedLevel AISpeed { get; set; }
 
@@ -90,5 +93,26 @@ namespace ProjetSynthese
             ////On applique la nouvelle rotation
             //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
+
+        private void MoveDestination(MoveTarget moveTarget)
+        {
+            float pas = this.currentSpeedLevel * Time.deltaTime;
+
+            ////La méthode MoveTowards de vector2 fait pas mal la job de déplacement pour nous.  Pour le moment elle va chercher la nouvelle position, mais le déplacement n'est pas encore fait.
+            //Vector2 nouvellePosition = Vector2.MoveTowards(transform.position, GetDestination(), pas);
+
+            ////Différence entre la nouvelle et l'ancienne position, pour calculer l'angle de rotation
+            //Vector2 mouvement = new Vector2(nouvellePosition.x - transform.position.x, nouvellePosition.y - transform.position.y);
+
+            ////Angle de rotation en degrée (car l'affichage de Unity veut les angles en degrée); Mathf.Rad2Deg nécessaire car Mathf.Atan2 un résultat en radiants
+            //float angle = -Mathf.Atan2(mouvement.x, mouvement.y) * Mathf.Rad2Deg;
+
+            ////On applique la nouvelle position
+            //transform.position = nouvellePosition;
+
+            ////On applique la nouvelle rotation
+            //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
     }
 }

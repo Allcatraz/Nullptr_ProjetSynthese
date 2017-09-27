c642141c7269bdef2c73ca0703d09c997eb6b85e
david <davidaupin@gmail.com>
2017-09-27 13:51:57 -0400

    Bar prefabs, boost et boost bar + controller

---
+++ Assets/Scripts/Playmode/Controller/BoostBarController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjetSynthese
+{
+    public class BoostBarController : GameScript
+    {
+        [SerializeField]
+        private Image image;
+        private BoostStats boostStats;
+        private float fillAmount;
+
+        private void Start()
+        {
+            boostStats = StaticBoostPass.boostStats;
+        }
+
+        private void Update()
+        {
+            SetFillAmountFromBoost();
+            UpdateBar();
+        }
+
+        private void SetFillAmountFromBoost()
+        {
+            if (boostStats != null)
+            {
+                fillAmount = boostStats.BoostPoints / boostStats.MaxBoostPoints;
+            }
+        }
+
+        private void UpdateBar()
+        {
+            image.fillAmount = fillAmount;
+        }
+    }
+}
+
+

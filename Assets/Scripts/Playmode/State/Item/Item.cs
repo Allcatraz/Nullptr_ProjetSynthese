2bd70588e791642e1b9fc6e74fcac1ec40b3a8b9
Félix <allcatraz123@gmail.com>
2017-09-28 14:27:47 -0400

    Les prefab d'items ont les bons enum d'associés, Ajout de prefabs d'items

--- Assets/Scripts/Playmode/State/Item/Item.cs
+++ Assets/Scripts/Playmode/State/Item/Item.cs
@@ -6,16 +6,31 @@ namespace ProjetSynthese
 {
     public abstract class Item : NetworkGameScript
     {
-        public ItemType Type { get; set; }
+        [SerializeField]
+        private ItemType type;
+
+        public ItemType Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
         public abstract void Use();
 
 
         public static bool operator==(Item item1, Item item2)
         {
             if ((object)item1 == null || (object)item2 == null)
+            {
                 return false;
-
-            return item1.Type == item2.Type;
+            }
+            return item1.type == item2.type;
         }
 
         public static bool operator !=(Item item1, Item item2)

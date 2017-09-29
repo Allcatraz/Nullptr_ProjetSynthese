7955da31dc052a5bd3e9762db3bfe9d1d2a8e18e
david <davidaupin@gmail.com>
2017-09-29 09:03:33 -0400

    Visual et logique weapon equipper

---
+++ Assets/Scripts/Playmode/State/UI/Equip/EquipWeapon.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjetSynthese
+{
+    public class EquipWeapon : GameScript
+    {
+        [SerializeField]
+        private EquipWeaponAt typeSlot;
+
+        [SerializeField]
+        private Text textName;
+
+        [SerializeField]
+        private Text textSlot;
+
+        private Inventory inventory;
+
+        private void UpdateInventory()
+        {
+            if (inventory != StaticInventoryPass.Inventory)
+            {
+                inventory = StaticInventoryPass.Inventory;
+            } 
+        }
+
+        private void UpdateText()
+        {
+            textSlot.text = typeSlot.ToString();
+            string name = "";
+            if (typeSlot == EquipWeaponAt.Primary)
+            {
+                name = inventory.GetPrimaryWeapon().GetItem().Type.ToString();
+            }
+            if (typeSlot == EquipWeaponAt.Secondary)
+            {
+                name = inventory.GetSecondaryWeapon().GetItem().Type.ToString();
+            }
+            textName.text = name;
+            
+        }
+
+        private void UpdateImage()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+            UpdateInventory();
+            UpdateText();
+            UpdateImage();
+        }
+    }
+}
+
+

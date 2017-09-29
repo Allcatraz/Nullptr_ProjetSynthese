4a354c8ff07c13b6cfa0bdbad784ffdfab81c87e
david <davidaupin@gmail.com>
2017-09-29 09:58:26 -0400

    UI pour item protection, pas de set d'image, mais methode en place.

---
+++ Assets/Scripts/Playmode/State/UI/Equip/EquipProtection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjetSynthese
+{
+    public class EquipProtection : GameScript
+    {
+
+
+        [SerializeField]
+        private ItemType type;
+
+        [SerializeField]
+        private Image iamge;
+
+        private Inventory inventory;
+
+        private void UpdateInventory()
+        {
+            if (inventory != StaticInventoryPass.Inventory)
+            {
+                inventory = StaticInventoryPass.Inventory;
+            }
+        }
+
+        private void UpdateImage()
+        {
+            if (type == ItemType.Vest)
+            {
+                Item vest = inventory.GetVest().GetItem();
+            }
+            if (type == ItemType.Helmet)
+            {
+                Item helmet = inventory.GetHelmet().GetItem();
+            }
+        }
+
+        void Update()
+        {
+            UpdateInventory();
+            UpdateImage();
+        }
+    }
+}
+

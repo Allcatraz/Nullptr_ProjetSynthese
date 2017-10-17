3f97ae2f0111239c5d83a68da9690839bcc79a41
Félix Lamontagne <allcatraz123@gmail.com>
2017-10-17 16:51:25 +0000

    Replace Weapon.cs

--- Assets/Scripts/Playmode/State/Item/Weapon.cs
+++ Assets/Scripts/Playmode/State/Item/Weapon.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ProjetSynthese
 {
+    public delegate void OnMunitionChanged();
+
     public class Weapon : Item
     {
         [SerializeField]
@@ -12,18 +12,54 @@ namespace ProjetSynthese
         private float bulletSpeed;
         [SerializeField]
         private float bulletLivingTime;
+        [SerializeField]
+        private int magazineMaxAmount;
+
+        public event OnMunitionChanged OnMunitionChanged;
+
+        private static int weight = 0;
+
+        public int MagazineMax
+        {
+            get { return magazineMaxAmount; }
+        }
+
+        public int MagazineAmount { get; set; }
 
         public float EffectiveWeaponRange
         {
-            get
-            {
-                return bulletLivingTime * bulletSpeed;
-            }
+            get { return bulletLivingTime * bulletSpeed; }
+        }
+
+        private void Awake()
+        {
+            MagazineAmount = MagazineMax;
         }
 
         public override void Use()
         {
-            BulletFactory.CreateBullet(bulletSpawnPoint, bulletSpeed, bulletLivingTime);
+            if (MagazineAmount > 0)
+            {
+                BulletFabric.CreateBullet(bulletSpawnPoint, bulletSpeed, bulletLivingTime);
+                MagazineAmount -= 1;
+                NotidyMunitionChanged();
+            }
+        }
+
+        public void Reload()
+        {
+            MagazineAmount = MagazineMax;
+            NotidyMunitionChanged();
+        }
+
+        private void NotidyMunitionChanged()
+        {
+            if (OnMunitionChanged != null) OnMunitionChanged();
+        }
+
+        public override int GetWeight()
+        {
+            return weight;
         }
     }
 }

844de90eb4a013b067ee930a492a1a61d6f1ef88
david <davidaupin@gmail.com>
2017-10-19 10:00:04 -0400

    Channel et publisher inclu dans l'editeur, pret au debugging

--- Assets/Scripts/Playmode/Controller/DroppedItemController.cs
+++ Assets/Scripts/Playmode/Controller/DroppedItemController.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Harmony;
 using UnityEngine;
 
-public class DroppedItemController : MonoBehaviour {
+namespace ProjetSynthese
+{
+    public class DroppedItemController : GameScript
+    {
+        private SpawnItemDropEventChannel spawnItemDropEventChannel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [SerializeField] private GameObject spawnLocation;
+
+        private void Awake()
+        {
+            InjectDependencies("InjectEventSensor");
+            spawnItemDropEventChannel.OnEventPublished += SpawnItemDropEventChannel_OnEventPublished;
+        }
+
+        private void SpawnItemDropEventChannel_OnEventPublished(SpawnItemDropEvent newEvent)
+        {
+            SpawnItem(newEvent.ItemToSpawn.GetItem().gameObject, newEvent.ItemToSpawn.GetItem().Player.transform);
+        }
+
+        private void InjectEventSensor([EventChannelScope] SpawnItemDropEventChannel spawnItemDropEventChannel)
+        {
+            this.spawnItemDropEventChannel = spawnItemDropEventChannel;
+        }
+
+        private void SpawnItem(GameObject itemToSpawn, Transform player)
+        {
+            itemToSpawn.SetActive(true);
+            itemToSpawn.transform.SetParent(spawnLocation.transform, false);
+            itemToSpawn.transform.position = player.position;
+        }
+    }
 }
+
+

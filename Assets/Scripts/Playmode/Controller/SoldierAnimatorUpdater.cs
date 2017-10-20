a65526eba807238bf1c6fa95957ad7016a834373
Félix <allcatraz123@gmail.com>
2017-10-20 10:46:14 -0400

    Modification de l'animation

--- Assets/Scripts/Playmode/Controller/SoldierAnimatorUpdater.cs
+++ Assets/Scripts/Playmode/Controller/SoldierAnimatorUpdater.cs
@@ -8,11 +8,10 @@ public class SoldierAnimatorUpdater : MonoBehaviour
     private Animator animator;
     public Vector3 MouvementDirection { get; set; }
     public Vector3 ViewDirection { get; set; }
+    private int leftHandLayer;
 
     public void UpdateAnimator()
     {
-    
-
         float angle = 0;
         float direction = Vector3.Dot(Vector3.Normalize(MouvementDirection), Vector3.Normalize(ViewDirection));
 
@@ -20,11 +19,13 @@ public class SoldierAnimatorUpdater : MonoBehaviour
         {
             direction = 1;
             angle = Vector3.SignedAngle(MouvementDirection, ViewDirection, Vector3.up);
+            //animator.SetLayerWeight(animator.GetLayerIndex("LeftHand"), 0);
         }
         else if (direction < 0)
         {
             direction = -1;
             angle = Vector3.SignedAngle(MouvementDirection * -1, ViewDirection, Vector3.up);
+            //animator.SetLayerWeight(animator.GetLayerIndex("LeftHand"), 1);
         }
         else
         {
@@ -33,5 +34,6 @@ public class SoldierAnimatorUpdater : MonoBehaviour
         angle /= 90;
         animator.SetInteger("Direction", (int)direction);
         animator.SetFloat("DirectionFactor", angle);
+
     }
 }

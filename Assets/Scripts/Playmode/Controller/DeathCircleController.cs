c4e5ba1c61a0d263bf1905682d63dab0050e4a3c
Lucas Desjardins <commandedede@hotmail.fr>
2017-11-17 07:51:28 -0500

    Timer donnant le temps restant avant que le deathCircle commence à rapetisser.

--- Assets/Scripts/Playmode/Controller/DeathCircleController.cs
+++ Assets/Scripts/Playmode/Controller/DeathCircleController.cs
@@ -1,4 +1,5 @@
-﻿using Harmony;
+﻿using System;
+using Harmony;
 using Tiled2Unity;
 using UnityEngine;
 
@@ -10,38 +11,51 @@ namespace ProjetSynthese
 
     public delegate void OnDeathCircleStatusUpdateEventHandler();
 
+    public delegate void OnDeathCircleTimeLeftEventHandler(int minutes, int seconds, bool finishWait);
+
     public class DeathCircleController : GameScript
     {
-        public enum Phases { Phase1, Phase2, Phase3, Phase4, Phase5, Phase6, Phase7, Phase8 }
+        public enum Phases
+        {
+            Phase1,
+            Phase2,
+            Phase3,
+            Phase4,
+            Phase5,
+            Phase6,
+            Phase7,
+            Phase8
+        }
 
         [Tooltip("Les informations sur les états du DeathCircle.")]
         [SerializeField]
         private DeathCircle deathCircleValues;
 
-        public DeathCircle DeathCircleValues { get { return deathCircleValues; } private set { deathCircleValues = value; } }
-
-        private const float InitialShrinkTime = 0.05f;
-        private const float InitialPlayerHurtTime = 1.0f;
-        private float waitTime;
-        private float moveTime;
-        private float playerHurtTime;
-        private float shrinkTime = InitialShrinkTime;
-        private float playerRadius;
-        private bool hasMoveTimeSet = false;
-        private bool isPlayerHit = false;
+        private const float SpeedDeathCircleShrink = 0.05f;
+        private const float SpeedPlayerHurtTime = 1.0f;
+        private float timeWait = 0;
+        private float timeBeforeMove = 0;
+        private float playerHurtTime = SpeedPlayerHurtTime;
+        private float timeShrinkDeathCircle = SpeedDeathCircleShrink;
+        private float playerRadius = 0;
+        private bool isSafeCircleSet = false;
+        private bool canPlayerGetHit = false;
 
         private PlayerMoveEventChannel playerMoveEventChannel;
         private LineRendererCircle safeCircle;
         private LineRendererCircle deathCircle;
-        public LineRendererCircle SafeCircle { get { return safeCircle; } private set { safeCircle = value; } }
-        public LineRendererCircle DeathCircle { get { return deathCircle; } private set { deathCircle = value; } }
         private TiledMap tiledMap;
         private Phases currentPhase = Phases.Phase1;
-        public Phases CurrentPhase { get { return currentPhase; } private set { currentPhase = value; } }
+
+        public DeathCircle DeathCircleValues { get { return deathCircleValues; } }
+        public LineRendererCircle SafeCircle { get { return safeCircle; } }
+        public LineRendererCircle DeathCircle { get { return deathCircle; } }
+        public Phases CurrentPhase { get { return currentPhase; } }
 
         public event OnPlayerHurtEventHandler OnPlayerHurt;
         public event OnDistanceChangeEventHandler OnDistanceChanged;
         public event OnDeathCircleStatusUpdateEventHandler OnFixedUpdate;
+        public event OnDeathCircleTimeLeftEventHandler OnTimeLeft;
 
         private void InjectDeathCircleController([SiblingsScope] TiledMap tiledMap,
                                                  [EventChannelScope] PlayerMoveEventChannel playerMoveEventChannel)
@@ -53,15 +67,15 @@ namespace ProjetSynthese
         private void Awake()
         {
             InjectDependencies("InjectDeathCircleController");
-            waitTime = deathCircleValues.WaitTimeInSecond[(int)currentPhase];
-            playerHurtTime = InitialPlayerHurtTime;
+            timeWait = deathCircleValues.WaitTimeInSecond[(int) currentPhase];
 
             safeCircle = GetAllChildrens()[0].GetComponent<LineRendererCircle>();
             deathCircle = GetAllChildrens()[1].GetComponent<LineRendererCircle>();
 
             playerMoveEventChannel.OnEventPublished += OnPlayerMove;
 
-            Vector2 tiledMapScaled = new Vector2((float)tiledMap.MapWidthInPixels / tiledMap.TileWidth, (float)tiledMap.MapHeightInPixels / tiledMap.TileHeight);
+            Vector2 tiledMapScaled = new Vector2((float) tiledMap.MapWidthInPixels / tiledMap.TileWidth,
+                                                 (float) tiledMap.MapHeightInPixels / tiledMap.TileHeight);
             transform.position = new Vector3(tiledMapScaled.x / 2f, 90, -tiledMapScaled.y / 2f);
             float mapDiagonalRadius = Mathf.Sqrt((tiledMapScaled.x * tiledMapScaled.x) + (tiledMapScaled.y * tiledMapScaled.y) / 2);
 
@@ -69,28 +83,25 @@ namespace ProjetSynthese
             CreateCircle(ref deathCircle, mapDiagonalRadius);
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
             if (currentPhase <= Phases.Phase8)
-            {
-                waitTime -= Time.deltaTime;
-
-                if (waitTime <= 0.0f)
+            {               
+                if (!isSafeCircleSet)
                 {
-                    if (!hasMoveTimeSet)
+                    timeBeforeMove = deathCircleValues.MoveTimeInSecond[(int) currentPhase];
+                    CreateCircle(ref safeCircle, safeCircle.Radius * deathCircleValues.Shrink[(int) currentPhase]);
+                    isSafeCircleSet = true;
+                }
+                
+                timeWait -= Time.deltaTime;
+                if (timeWait <= 0.0f)
+                {
+                    timeBeforeMove -= Time.deltaTime;
+                    if (timeBeforeMove <= 0.0f)
                     {
-                        moveTime = deathCircleValues.MoveTimeInSecond[(int)currentPhase];
-                        CreateCircle(ref safeCircle, safeCircle.Radius * deathCircleValues.Shrink[(int)currentPhase]);
-
-                        hasMoveTimeSet = true;
-                    }
-
-                    moveTime -= Time.deltaTime;
-
-                    if (moveTime <= 0.0f)
-                    {
-                        shrinkTime -= Time.deltaTime;
-                        if (shrinkTime <= 0.0f)
+                        timeShrinkDeathCircle -= Time.deltaTime;
+                        if (timeShrinkDeathCircle <= 0.0f)
                         {
                             deathCircle.Radius -= 0.2f;
                             deathCircle.Create();
@@ -103,29 +114,30 @@ namespace ProjetSynthese
                                 {
                                     currentPhase += 1;
                                 }
-                                waitTime = deathCircleValues.WaitTimeInSecond[(int)currentPhase];
-                                hasMoveTimeSet = false;
+                                timeWait = deathCircleValues.WaitTimeInSecond[(int) currentPhase];
+                                isSafeCircleSet = false;
                                 return;
                             }
 
-                            shrinkTime = InitialShrinkTime;
+                            timeShrinkDeathCircle = SpeedDeathCircleShrink;
                         }
                     }
                 }
+                CalculateTime();
             }
 
             // à faire si le joueur est à l'extérieur du cercle
-            if (isPlayerHit)
+            if (canPlayerGetHit)
             {
                 playerHurtTime -= Time.deltaTime;
                 if (playerHurtTime <= 0.0f)
-                {
-                    playerHurtTime = InitialPlayerHurtTime;
+                {                    
                     if (OnPlayerHurt != null) OnPlayerHurt(deathCircleValues.DomagePerSecond[(int) currentPhase]);
+                    playerHurtTime = SpeedPlayerHurtTime;
                 }
             }
 
-            if (OnFixedUpdate != null) OnFixedUpdate(); 
+            if (OnFixedUpdate != null) OnFixedUpdate();
         }
 
         private void OnPlayerMove(PlayerMoveEvent playerMoveEvent)
@@ -133,7 +145,7 @@ namespace ProjetSynthese
             float distancePlayerFromSafeCircle = Mathf.Sqrt(Mathf.Pow(deathCircle.transform.position.x - playerMoveEvent.PlayerMover.transform.position.x, 2) +
                                                             Mathf.Pow(deathCircle.transform.position.z - playerMoveEvent.PlayerMover.transform.position.z, 2));
             playerRadius = distancePlayerFromSafeCircle;
-            isPlayerHit = distancePlayerFromSafeCircle > deathCircle.Radius;
+            canPlayerGetHit = distancePlayerFromSafeCircle > deathCircle.Radius;
         }
 
         private void CreateCircle(ref LineRendererCircle lineRendererCircle, float radius)
@@ -141,6 +153,15 @@ namespace ProjetSynthese
             lineRendererCircle.Radius = radius;
             lineRendererCircle.Create();
         }
+
+        private void CalculateTime()
+        {
+            bool isWaitFinish = timeWait <= 0;
+            timeWait = timeWait <= 0 ? 0 : timeWait;
+            int time = (int)(timeWait + timeBeforeMove);
+            int minutes = time / 60 >= 0 ? time / 60 : 0;
+            int seconds = time % 60 >= 0 ? time % 60 : 0;
+            if (OnTimeLeft != null) OnTimeLeft(minutes, seconds, isWaitFinish);
+        }
     }
 }
-
---
+++ Assets/Scripts/Playmode/Event/DeathCircleTimeLeftEventPublisher.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace ProjetSynthese
+{
+    [AddComponentMenu("Game/Event/DeathCircleTimeLeftEventPublisher")]
+    public class DeathCircleTimeLeftEventPublisher : GameScript
+    {
+        private DeathCircleController deathCircleController;
+        private DeathCircleTimeLeftEventChannel deathCircleTimeLeftEventChannel;
+
+        private void InjectDeathCircleTimeLeftEventPublisher([GameObjectScope] DeathCircleController deathCircleController,
+                                                             [EventChannelScope] DeathCircleTimeLeftEventChannel deathCircleTimeLeftEventChannel)
+        {
+            this.deathCircleController = deathCircleController;
+            this.deathCircleTimeLeftEventChannel = deathCircleTimeLeftEventChannel;
+        }
+
+        private void Awake()
+        {
+            InjectDependencies("InjectDeathCircleTimeLeftEventPublisher");    
+        }
+
+        private void OnEnable()
+        {
+            deathCircleController.OnTimeLeft += OnTimeLeft;
+        }
+
+        private void OnDisable()
+        {
+            deathCircleController.OnTimeLeft -= OnTimeLeft;
+        }
+
+        private void OnTimeLeft(int minutes, int seconds, bool isWaitFinish)
+        {
+            deathCircleTimeLeftEventChannel.Publish(new DeathCircleTimeLeftEvent(minutes, seconds, isWaitFinish));
+        }
+    }
+}

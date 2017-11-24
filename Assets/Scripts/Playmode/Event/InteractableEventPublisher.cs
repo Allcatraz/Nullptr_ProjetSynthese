d26d9d67990794ff81e478c2b664bf8b1f36805b
Lucas Desjardins <commandedede@hotmail.fr>
2017-11-24 08:47:55 -0500

    Nous avons un text qui est afficher à l'écran lorsque quelque chose que le joueur peut interagir avec est proche.

---
+++ Assets/Scripts/Playmode/Event/InteractableEventPublisher.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace ProjetSynthese
+{
+    public class InteractableEventPublisher : GameScript
+    {
+        private InteractableSensor interactableSensor;
+        private InteractableEventChannel interactableEventChannel;
+
+        private void InjectInteractableEventPublisher([GameObjectScope] InteractableSensor interactableSensor,
+                                                      [EventChannelScope] InteractableEventChannel interactableEventChannel)
+        {
+            this.interactableSensor = interactableSensor;
+            this.interactableEventChannel = interactableEventChannel;
+        }
+
+        private void Awake()
+        {
+            InjectDependencies("InjectInteractableEventPublisher");
+            interactableSensor.OnInteratableFound += OnInteractableFound;
+        }
+
+        private void OnDestroy()
+        {
+            interactableSensor.OnInteratableFound -= OnInteractableFound;
+        }
+
+        private void OnInteractableFound(bool foundInteractible)
+        {
+            interactableEventChannel.Publish(new InteractableEvent(foundInteractible));
+        }
+    }
+}

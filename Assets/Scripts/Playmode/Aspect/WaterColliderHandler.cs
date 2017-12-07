7084738889d7c4062e69fe8587738d1b09357166
david <davidaupin@gmail.com>
2017-12-07 10:03:16 -0500

    Water supporte plusieurs personnes

--- Assets/Scripts/Playmode/Aspect/WaterColliderHandler.cs
+++ Assets/Scripts/Playmode/Aspect/WaterColliderHandler.cs
@@ -7,47 +7,71 @@ namespace ProjetSynthese
 {
     public class WaterColliderHandler : GameScript
     {
-        private Collision playerCollider;
+        private List<Collision> listPlayerCollision;
 
         private void OnCollisionEnter(Collision col)
         {
-            playerCollider = col;
+            if (listPlayerCollision == null)
+            {
+                listPlayerCollision = new List<Collision>();
+            }
+            if (!CheckIfExistInList(col)) listPlayerCollision.Add(col);
+        }
+
+        private bool CheckIfExistInList(Collision col)
+        {
+            bool exist = false;
+            foreach (Collision colli in listPlayerCollision)
+            {
+                if (colli == col)
+                {
+                    exist = true;
+                    break;
+                }
+            }
+            return exist;
         }
 
         private void FixedUpdate()
-        {            
-            if (playerCollider != null)
+        {
+            if (listPlayerCollision != null)
             {
-                RaycastHit info;
-                Vector3 origin = playerCollider.gameObject.transform.position;
-                origin.y += 3;
-                bool isTrigger = Physics.Raycast(origin, Vector3.down, out info, 10, 1 << LayerMask.NameToLayer(R.S.Layer.Water));
-                Debug.DrawRay(origin, Vector3.down, Color.red);
-                if (isTrigger)
+                foreach (Collision playerCollider in listPlayerCollision)
                 {
-                    ISwim actorController = playerCollider.gameObject.GetComponent<PlayerController>();
-                    if (actorController == null)
+                    if (playerCollider != null)
                     {
-                        actorController = playerCollider.gameObject.GetComponentInParent<ActorAI>();
-                    }
-                    if (actorController != null && actorController.IsSwimming == false)
-                    {
-                        actorController.IsSwimming = true;
+                        RaycastHit info;
+                        Vector3 origin = playerCollider.gameObject.transform.position;
+                        origin.y += 3;
+                        bool isTrigger = Physics.Raycast(origin, Vector3.down, out info, 10, 1 << LayerMask.NameToLayer(R.S.Layer.Water));
+                        Debug.DrawRay(origin, Vector3.down, Color.red);
+                        if (isTrigger)
+                        {
+                            ISwim actorController = playerCollider.gameObject.GetComponent<PlayerController>();
+                            if (actorController == null)
+                            {
+                                actorController = playerCollider.gameObject.GetComponentInParent<ActorAI>();
+                            }
+                            if (actorController != null && actorController.IsSwimming == false)
+                            {
+                                actorController.IsSwimming = true;
+                            }
+                        }
+                        else
+                        {
+                            ISwim actorController = playerCollider.gameObject.GetComponent<PlayerController>();
+                            if (actorController == null)
+                            {
+                                actorController = playerCollider.gameObject.GetComponentInParent<ActorAI>();
+                            }
+                            if (actorController != null && actorController.IsSwimming)
+                            {
+                                actorController.IsSwimming = false;
+                            }
+                        }
                     }
                 }
-                else
-                {
-                    ISwim actorController = playerCollider.gameObject.GetComponent<PlayerController>();
-                    if (actorController == null)
-                    {
-                        actorController = playerCollider.gameObject.GetComponentInParent<ActorAI>();
-                    }
-                    if (actorController != null && actorController.IsSwimming)
-                    {
-                        actorController.IsSwimming = false;
-                    }
-                }
-            }
+            }  
         }
     }
 }
